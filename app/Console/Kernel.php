b0a95f5920f3504b51666211684bde48e660354d
Marco Marassi <thtg88@users.noreply.github.com>
2021-05-08 19:09:25 +0100

    Run telescope:prune for the past 24 hours every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('horizon:snapshot')->everyMinute();
+        $schedule->command('telescope:prune --hours=24')->everyMinute();
     }
 
     /**
