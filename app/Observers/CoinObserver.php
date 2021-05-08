68bf7c83b1d7b9a69984353964f1b381ac3c30d7
Marco Marassi <thtg88@users.noreply.github.com>
2021-05-08 17:20:26 +0100

    Don't fetch price if coin not enabled

--- app/Observers/CoinObserver.php
+++ app/Observers/CoinObserver.php
@@ -16,6 +16,10 @@ class CoinObserver
      */
     public function created(Coin $coin): void
     {
+        if (!in_array($coin->external_id, config('app.enabled_coins'))) {
+            return;
+        }
+
         $currencies = Currency::select('symbol')->pluck('symbol')->toArray();
 
         dispatch(new FetchCoinPriceJob($coin->external_id, $currencies));
