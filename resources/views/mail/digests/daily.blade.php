47759d9414c2c315b6ea3e6e113d268010e6f8b2
Marco Marassi <thtg88@users.noreply.github.com>
2021-05-12 20:04:37 +0100

    Separated average and min-max tables in daily digest notification view

--- resources/views/mail/digests/daily.blade.php
+++ resources/views/mail/digests/daily.blade.php
@@ -4,11 +4,23 @@
 Here's your daily {{ config('app.name') }} update,
 from {{ $start->toDateTimeString() }} to {{ $end->toDateTimeString() }}.
 
+## Average
+
 @component('mail::table')
-| Coin | Min | Max | Start | End | Trend |
-|------|----:|----:|------:|----:|:-----:|
+| Coin | Trend | Start | End |
+|------|:-----:|------:|----:|
 @foreach ($data as $row)
-| {{ $row['coin'] }} | {{ $row['min'] }} | {{ $row['max']}} | {{ $row['first'] }} | {{ $row['last'] }} | {{ $row['trend'] }}
+| {{ $row['coin'] }} | {{ $row['trend'] }} | {{ $row['first'] }} | {{ $row['last'] }} |
+@endforeach
+@endcomponent
+
+## Min-Max
+
+@component('mail::table')
+| Coin | Min | Max |
+|------|----:|----:|
+@foreach ($data as $row)
+| {{ $row['coin'] }} | {{ $row['min'] }} | {{ $row['max']}} |
 @endforeach
 @endcomponent
 
