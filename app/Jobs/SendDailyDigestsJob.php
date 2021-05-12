7843a6d304387ddb52a8aa7a033ae03876ea1e90
Marco Marassi <thtg88@users.noreply.github.com>
2021-05-12 19:35:44 +0100

    Simplify send daily digest job logic

--- app/Jobs/SendDailyDigestsJob.php
+++ app/Jobs/SendDailyDigestsJob.php
@@ -11,11 +11,6 @@ class SendDailyDigestsJob extends Job
 {
     public function handle(): void
     {
-        // do not resend if there already alerts sent for this day
-        if ($this->shouldntSend()) {
-            return;
-        }
-
         foreach ($this->alerts() as $alert) {
             $alert->user->notify(new DailyDigestNotification(
                 now()->copy()->setTime(8, 0)->subDays(1),
@@ -26,16 +21,10 @@ class SendDailyDigestsJob extends Job
         }
     }
 
-    private function shouldntSend(): bool
-    {
-        // If there are existing alerts sent today
-        return Alert::where('type', 'daily-digest')
-            ->whereDate('last_sent_at', now()->toDateString())
-            ->exists();
-    }
-
     private function alerts(): Collection
     {
-        return Alert::where('type', 'daily-digest')->get();
+        return Alert::where('type', 'daily-digest')
+            ->whereDate('last_sent_at', now()->toDateString())
+            ->get();
     }
 }
