c8333d51745d9333a1f38c4a880826af7db12d12
Marco Marassi <thtg88@users.noreply.github.com>
2021-05-15 12:18:39 +0100

    Refactor daily digest notification to abstract digest notification

--- app/Notifications/DailyDigestNotification.php
+++ app/Notifications/DailyDigestNotification.php
@@ -6,115 +6,22 @@ use App\Models\Average;
 use App\Models\Coin;
 use App\Models\Currency;
 use App\Models\User;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
-use Illuminate\Support\Carbon;
 
-class DailyDigestNotification extends Notification implements ShouldQueue
+class DailyDigestNotification extends DigestNotification
 {
-    use Queueable;
-
-    public function __construct(private Carbon $start, private Carbon $end)
-    {
-    }
-
-    public function via(User $notifiable)
-    {
-        return ['mail'];
-    }
-
     public function toMail(User $notifiable): MailMessage
     {
-        $data = [];
-        foreach ($this->coins() as $coin) {
-            foreach ($this->currencies() as $currency) {
-                $data[] = $this->getRow($coin, $currency);
-            }
-        }
-
         return (new MailMessage)->markdown('mail.digests.daily', [
-            'data' => $data,
+            'data' => $this->getData(),
             'notifiable' => $notifiable,
             'start' => $this->start,
             'end' => $this->end,
         ]);
     }
 
-    private function getRow(Coin $coin, Currency $currency): array
-    {
-        $first = $this->format($this->firstHourlyAverage($coin, $currency));
-        $last = $this->format($this->lastHourlyAverage($coin, $currency));
-        if ($last > $first) {
-            $trend = '✅';
-        } else {
-            $trend = '❌';
-        }
-
-        return [
-            'coin' => $coin->external_id,
-            'currency' => $currency->symbol,
-            'min' => $this->format($this->minHourlyAverage($coin, $currency)),
-            'max' => $this->format($this->maxHourlyAverage($coin, $currency)),
-            'first' => $first,
-            'last' => $last,
-            'trend' => $trend,
-        ];
-    }
-
-    private function format(float $value): string
-    {
-        if ($value >= 1000) {
-            return number_format($value, 2);
-        }
-
-        return (string) $value;
-    }
-
-    private function coins(): Collection
-    {
-        return Coin::enabled()->select('id', 'external_id')->get();
-    }
-
-    private function currencies(): Collection
-    {
-        return Currency::select('id', 'symbol')->get();
-    }
-
-    private function maxHourlyAverage(Coin $coin, Currency $currency): float
-    {
-        return $this->hourlyAveragesQuery($coin, $currency)
-            ->max('value') / config('app.coin_storage_coefficient');
-    }
-
-    private function minHourlyAverage(Coin $coin, Currency $currency): float
-    {
-        return $this->hourlyAveragesQuery($coin, $currency)
-            ->min('value') / config('app.coin_storage_coefficient');
-    }
-
-    private function firstHourlyAverage(Coin $coin, Currency $currency): float
-    {
-        return $this->hourlyAveragesQuery($coin, $currency)
-            ->orderBy('from')
-            ->select('value')
-            ->first()
-            ?->value ?? 0.0;
-    }
-
-    private function lastHourlyAverage(Coin $coin, Currency $currency): float
-    {
-        return $this->hourlyAveragesQuery($coin, $currency)
-            ->orderByDesc('to')
-            ->select('value')
-            ->first()
-            ?->value ?? 0.0;
-    }
-
-    private function hourlyAveragesQuery(Coin $coin, Currency $currency): Builder
+    protected function averageQuery(Coin $coin, Currency $currency): Builder
     {
         return Average::where('from', '>=', $this->start->toDateTimeString())
             ->where('to', '<=', $this->end->toDateTimeString())
---
+++ app/Notifications/DigestNotification.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Coin;
+use App\Models\Currency;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Carbon;
+
+abstract class DigestNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(protected Carbon $start, protected Carbon $end)
+    {
+    }
+
+    public function via(User $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    protected function getData(): array
+    {
+        $data = [];
+
+        foreach ($this->coins() as $coin) {
+            foreach ($this->currencies() as $currency) {
+                $data[] = $this->getRow($coin, $currency);
+            }
+        }
+
+        return $data;
+    }
+
+    protected function getRow(Coin $coin, Currency $currency): array
+    {
+        $first = $this->firstAverage($coin, $currency);
+        $last = $this->lastAverage($coin, $currency);
+
+        return [
+            'coin' => $coin->external_id,
+            'currency' => $currency->symbol,
+            'min' => $this->format($this->minAverage($coin, $currency)),
+            'max' => $this->format($this->maxAverage($coin, $currency)),
+            'first' => $this->format($first),
+            'last' => $this->format($last),
+            'trend' => $this->getTrend($first, $last),
+            'variation_percentage' => $this->getVariationPercentage($first, $last),
+        ];
+    }
+
+    protected function getVariationPercentage(float $first_value, float $last_value): string
+    {
+        $variation = $last_value - $first_value;
+        $percentage = ($variation / $first_value) * 100;
+
+        return number_format($percentage, 2);
+    }
+
+    protected function getTrend(float $first_value, float $last_value): string
+    {
+        if ($last_value > $first_value) {
+            return '🟢';
+        }
+
+        return '🛑';
+    }
+
+    protected function format(float $value): string
+    {
+        if ($value >= 1000) {
+            return number_format($value, 2);
+        }
+
+        return (string) $value;
+    }
+
+    protected function coins(): Collection
+    {
+        return Coin::enabled()->select('id', 'external_id')->get();
+    }
+
+    protected function currencies(): Collection
+    {
+        return Currency::select('id', 'symbol')->get();
+    }
+
+    protected function maxAverage(Coin $coin, Currency $currency): float
+    {
+        return $this->averageQuery($coin, $currency)
+            ->max('value') / config('app.coin_price_storage_coefficient');
+    }
+
+    protected function minAverage(Coin $coin, Currency $currency): float
+    {
+        return $this->averageQuery($coin, $currency)
+            ->min('value') / config('app.coin_price_storage_coefficient');
+    }
+
+    protected function firstAverage(Coin $coin, Currency $currency): float
+    {
+        return $this->averageQuery($coin, $currency)
+            ->orderBy('from')
+            ->select('value')
+            ->first()
+            ?->value ?? 0.0;
+    }
+
+    protected function lastAverage(Coin $coin, Currency $currency): float
+    {
+        return $this->averageQuery($coin, $currency)
+            ->orderByDesc('to')
+            ->select('value')
+            ->first()
+            ?->value ?? 0.0;
+    }
+
+    abstract protected function averageQuery(Coin $coin, Currency $currency): Builder;
+}
