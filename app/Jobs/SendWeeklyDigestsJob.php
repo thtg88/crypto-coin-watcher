bb4b62f8b24dd0a5dd02ba2960ec7579649d107f
Marco Marassi <thtg88@users.noreply.github.com>
2021-06-05 20:16:13 +0100

    Removed additional logging in send weekly digests job

--- app/Jobs/SendWeeklyDigestsJob.php
+++ app/Jobs/SendWeeklyDigestsJob.php
@@ -7,7 +7,6 @@ use App\Notifications\WeeklyDigestNotification;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 
 class SendWeeklyDigestsJob extends Job
 {
@@ -15,19 +14,13 @@ class SendWeeklyDigestsJob extends Job
     {
         $alerts = $this->alerts();
 
-        Log::debug("{$alerts->count()} weekly alerts found");
-
         foreach ($this->alerts() as $alert) {
-            Log::debug("Sending weekly alert to {$alert->user->name} ({$alert->user->email})...");
-
             $alert->user->notify(new WeeklyDigestNotification(
                 now()->copy()->setTime(9, 0)->subDays(7),
                 now()->copy()->setTime(9, 0),
             ));
 
             $alert->update(['last_sent_at' => now()->toDateTimeString()]);
-
-            Log::debug("Sent weekly alert to {$alert->user->name} ({$alert->user->email})");
         }
     }
 
